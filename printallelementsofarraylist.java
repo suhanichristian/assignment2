9f2130e11854cdef7f4911604b14bebe46d680f0
suhanichristian <143085822+suhanichristian@users.noreply.github.com>
2023-09-14 16:23:30 +0530

    Update printallelementsofarraylist.java

--- printallelementsofarraylist.java
+++ printallelementsofarraylist.java
@@ -8,7 +8,18 @@ import java.util.*;
 
 public class printallelementsofarraylist {
 	public static void main(String[] args) {
-		
-	}
+		ArrayList<String> arrayList = new ArrayList<>();
 
+        // Add elements to the ArrayList
+        arrayList.add("First");
+        arrayList.add("Second");
+        arrayList.add("Third");
+        arrayList.add("Fourth");
+
+        // Print all elements with their positions
+        for (int i = 0; i < arrayList.size(); i++) {
+            String element = arrayList.get(i);
+            System.out.println("Position " + i + ": " + element);
+        }
+	}
 }
